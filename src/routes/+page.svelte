8b5204174264916e91cc934a415fc8684bfa1a5d
Jason Herring <jason@ordinarykids.com>
2023-10-23 20:49:27 -0700

    updated url

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 
 
 
-<iframe width="1280" height="720" style="background-color: white;" src="https://www.youtube.com/embed/f_U5cwGFfME" title="stylegan2 x op art patterns moires   latent walks 09 15 2020 1080p 2" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+<iframe width="1280" height="720" style="background-color: white;" src="https://www.youtube.com/embed/f_U5cwGFfME?loop=0&list=PLLNx8st5zQAqNJ8QPYO0bidkVA3heg12X" title="stylegan2 x op art patterns moires   latent walks 09 15 2020 1080p 2" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
